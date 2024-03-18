d2498649a8258d49da6a53fd79d03349ec677dcd
Pest270904 <ntpllc2004@gmail.com>
2024-03-18 08:45:43 +0700

    Add project files.

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(58, 82);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(139, 58);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Đọc File";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(252, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(501, 315);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWrite.Location = new System.Drawing.Point(58, 180);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(139, 58);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "Ghi File";
+            this.btnWrite.UseVisualStyleBackColor = false;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(58, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 58);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(816, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Ghi và đọc file";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.ShowDialog();
+                FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+                StreamReader sr = new StreamReader(fs);
+
+                string content = sr.ReadToEnd();
+                richTextBox1.Text = content;
+                fs.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Wrong path");
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.ShowDialog();
+                FileStream fs = new FileStream(sfd.FileName, FileMode.OpenOrCreate);
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.Write(richTextBox1.Text.ToUpper().Trim());
+                }
+                fs.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Wrong path");
+            }
+        }
+    }
+}
