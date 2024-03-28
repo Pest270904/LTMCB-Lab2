61823c9ea06665cea0d58b9ad39678110d25245a
Khoi <khoatran.9048@gmail.com>
2024-03-28 11:01:07 +0700

    add task4 to github

--- Lab2/Bai_04.Designer.cs
+++ Lab2/Bai_04.Designer.cs
@@ -28,12 +28,293 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.column_mssv = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column_hoten = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column_sdt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column_toan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column_van = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.bt_mo_file_output = new System.Windows.Forms.Button();
+            this.bt_exit = new System.Windows.Forms.Button();
+            this.bt_xoa = new System.Windows.Forms.Button();
+            this.bt_tinhdtb = new System.Windows.Forms.Button();
+            this.bt_addsv = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(634, 275);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập thông tin sinh viên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(395, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(512, 162);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(496, 61);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(241, 178);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(232, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(44, 375);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(634, 155);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.column_mssv,
+            this.column_hoten,
+            this.column_sdt,
+            this.column_toan,
+            this.column_van});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(606, 111);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // column_mssv
+            // 
+            this.column_mssv.Text = "MSSV";
+            // 
+            // column_hoten
+            // 
+            this.column_hoten.Text = "Họ Và Tên";
+            this.column_hoten.Width = 120;
+            // 
+            // column_sdt
+            // 
+            this.column_sdt.Text = "SĐT";
+            this.column_sdt.Width = 120;
+            // 
+            // column_toan
+            // 
+            this.column_toan.Text = "Điểm Toán";
+            this.column_toan.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.column_toan.Width = 120;
+            // 
+            // column_van
+            // 
+            this.column_van.Text = "Điểm Văn";
+            this.column_van.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.column_van.Width = 100;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.bt_mo_file_output);
+            this.groupBox3.Controls.Add(this.bt_exit);
+            this.groupBox3.Controls.Add(this.bt_xoa);
+            this.groupBox3.Controls.Add(this.bt_tinhdtb);
+            this.groupBox3.Controls.Add(this.bt_addsv);
+            this.groupBox3.Location = new System.Drawing.Point(790, 44);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(404, 275);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chức năng";
+            // 
+            // bt_mo_file_output
+            // 
+            this.bt_mo_file_output.Location = new System.Drawing.Point(270, 22);
+            this.bt_mo_file_output.Name = "bt_mo_file_output";
+            this.bt_mo_file_output.Size = new System.Drawing.Size(75, 23);
+            this.bt_mo_file_output.TabIndex = 4;
+            this.bt_mo_file_output.Text = "Mở File OutPut";
+            this.bt_mo_file_output.UseVisualStyleBackColor = true;
+            this.bt_mo_file_output.Click += new System.EventHandler(this.bt_mo_file_output_Click);
+            // 
+            // bt_exit
+            // 
+            this.bt_exit.Location = new System.Drawing.Point(270, 160);
+            this.bt_exit.Name = "bt_exit";
+            this.bt_exit.Size = new System.Drawing.Size(75, 23);
+            this.bt_exit.TabIndex = 3;
+            this.bt_exit.Text = "Thoát";
+            this.bt_exit.UseVisualStyleBackColor = true;
+            this.bt_exit.Click += new System.EventHandler(this.bt_exit_Click);
+            // 
+            // bt_xoa
+            // 
+            this.bt_xoa.Location = new System.Drawing.Point(270, 98);
+            this.bt_xoa.Name = "bt_xoa";
+            this.bt_xoa.Size = new System.Drawing.Size(75, 23);
+            this.bt_xoa.TabIndex = 2;
+            this.bt_xoa.Text = "Xóa";
+            this.bt_xoa.UseVisualStyleBackColor = true;
+            this.bt_xoa.Click += new System.EventHandler(this.bt_xoa_Click);
+            // 
+            // bt_tinhdtb
+            // 
+            this.bt_tinhdtb.Location = new System.Drawing.Point(60, 154);
+            this.bt_tinhdtb.Name = "bt_tinhdtb";
+            this.bt_tinhdtb.Size = new System.Drawing.Size(111, 23);
+            this.bt_tinhdtb.TabIndex = 1;
+            this.bt_tinhdtb.Text = "Tính ĐTB";
+            this.bt_tinhdtb.UseVisualStyleBackColor = true;
+            this.bt_tinhdtb.Click += new System.EventHandler(this.bt_tinhdtb_Click);
+            // 
+            // bt_addsv
+            // 
+            this.bt_addsv.Location = new System.Drawing.Point(51, 67);
+            this.bt_addsv.Name = "bt_addsv";
+            this.bt_addsv.Size = new System.Drawing.Size(75, 23);
+            this.bt_addsv.TabIndex = 0;
+            this.bt_addsv.Text = "thêm sv";
+            this.bt_addsv.UseVisualStyleBackColor = true;
+            this.bt_addsv.Click += new System.EventHandler(this.bt_addsv_Click);
+            // 
+            // Bai_04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1239, 542);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Bai_04";
             this.Text = "Bai_04";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader column_mssv;
+        private System.Windows.Forms.ColumnHeader column_hoten;
+        private System.Windows.Forms.ColumnHeader column_sdt;
+        private System.Windows.Forms.ColumnHeader column_toan;
+        private System.Windows.Forms.ColumnHeader column_van;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button bt_mo_file_output;
+        private System.Windows.Forms.Button bt_exit;
+        private System.Windows.Forms.Button bt_xoa;
+        private System.Windows.Forms.Button bt_tinhdtb;
+        private System.Windows.Forms.Button bt_addsv;
     }
 }
--- Lab2/Bai_04.cs
+++ Lab2/Bai_04.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace Lab2
 {
     public partial class Bai_04 : Form
@@ -15,6 +18,45 @@ namespace Lab2
         public Bai_04()
         {
             InitializeComponent();
+        }
+        List<Student> students = new List<Student>();
+
+        private void bt_addsv_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadData()
+        {
+            listView1.Items.Clear();
+            foreach (Student student in students)
+            {
+                ListViewItem item = new ListViewItem(student.MSSV);
+                item.SubItems.Add(student.HoTen);
+                item.SubItems.Add(student.SDT);
+                item.SubItems.Add(student.DToan.ToString());
+                item.SubItems.Add(student.DVan.ToString());
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void bt_tinhdtb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_mo_file_output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_xoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
